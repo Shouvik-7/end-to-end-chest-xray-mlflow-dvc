81ff1f8ad93308220fdab2c963c4643ec5807c57
Shouvik <shouvik1999@gmail.com>
2024-05-15 14:19:53 -0600

    base model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from chestxrayClassifier import logger
 from chestxrayClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from chestxrayClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -13,3 +13,24 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e 
+
+
+
+from chestxrayClassifier.config.configuration import ConfigurationManager
+from chestxrayClassifier.components.prepare_model import PrepareBaseModel 
+from chestxrayClassifier import logger 
+
+
+STAGE_NAME = 'Prepare base model'
+
+try: 
+    logger.info(f"************")
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx===========x")
+
+except Exception as e: 
+    logger.exception(e)
+    raise e 
+    
